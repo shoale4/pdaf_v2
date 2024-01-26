7251e2e619c54a7e232b260a54676436ef0b8f4b
shoale4 <shoale4@gmail.com>
2024-01-25 20:21:35 -0600

    one small addition (line 23) to macOS shell script

--- build_and_run_macos.sh
+++ build_and_run_macos.sh
@@ -20,6 +20,7 @@ make model
 cd tools
 make clean
 gfortran -c -o parser_no_mpi.o parser_no_mpi.F90
+gfortran -c -o output_txt.o output_txt.F90
 make all
 ./generate_ens -ens_size 4
 ./generate_obs -obs_choice 1 -obs_spacing 6
